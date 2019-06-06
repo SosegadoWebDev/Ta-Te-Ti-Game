9d106f90dd6d9cf75ea0a8a1e6e17e3339fac804
Jaime González <arcarius11@gmail.com>
2019-06-06 13:54:57 -0300

    estructura del juego

--- js/script.js
+++ js/script.js
@@ -1,8 +1,5 @@
-/*$(document).ready(function () {
-    console.log("Funciona")
-});
-console.log($("#parrafo").text())
-$("#parrafo").on("click", function () {
-    console.log($(this).text());
-});*/
+$(document).ready(function(){
+    //console.log("Ready")
+    
+})
 
